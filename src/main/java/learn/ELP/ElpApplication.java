12318cfdef3f5f3377631502eeb0d1fbbbbe6f77
binh1922004 <binh1920042018@gmail.com>
2024-12-28 23:00:58 +0700

    init

---
+++ src/main/java/learn/ELP/ElpApplication.java
@@ -0,0 +1,13 @@
+package learn.ELP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learn/ELP/ElpApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.ELP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
