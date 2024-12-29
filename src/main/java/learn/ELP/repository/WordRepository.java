c29f76f737ccacc073b84dc332183ff31d9286dc
binh1922004 <binh1920042018@gmail.com>
2024-12-29 21:15:14 +0700

    add word repository

---
+++ src/main/java/learn/ELP/repository/WordRepository.java
@@ -0,0 +1,9 @@
+package learn.ELP.repository;
+
+import learn.ELP.entity.Word;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WordRepository extends JpaRepository<Word, Long> {
+}
