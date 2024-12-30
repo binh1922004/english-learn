231d8761a56d77cd71f414a62bbd4fd8e52d8906
binh1922004 <binh1920042018@gmail.com>
2024-12-30 17:19:15 +0700

    add login form

---
+++ frontend/src/component/AppContent.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Buttons from "./Buttons";
+export default class AppContent extends React.Component{
+
+    render() {
+        return (
+            <>
+              <Buttons
+              />
+            </>
+        )
+    }
+}
