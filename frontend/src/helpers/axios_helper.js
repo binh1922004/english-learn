c1e655cf1fb7c0a5c50e1e2a434b39c3dd11c729
binh1922004 <binh1920042018@gmail.com>
2024-12-30 23:37:49 +0700

    modify to add bearer header

--- frontend/src/helpers/axios_helper.js
+++ frontend/src/helpers/axios_helper.js
@@ -18,9 +18,9 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 export const request = (method, url, data) => {
 
     let headers = {};
-    // if (getAuthToken() !== null && getAuthToken() !== "null") {
-    //     headers = {'Authorization': `Bearer ${getAuthToken()}`};
-    // }
+    if (getAuthToken() !== null && getAuthToken() !== "null") {
+        headers = {'Authorization': `Bearer ${getAuthToken()}`};
+    }
 
     return axios({
         method: method,
