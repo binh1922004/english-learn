72677b424cc8b044ba0a6928184db7f2ee3b034d
binh1922004 <binh1920042018@gmail.com>
2025-01-06 00:44:28 +0700

    modify to HashRouter

--- frontend/src/component/AppContent.js
+++ frontend/src/component/AppContent.js
@@ -36,7 +36,7 @@ class AppContent extends Component{
             (response) => {
                 setAuthHeader(response.data.result.token)
                 this.setState({componentToShow: "login"})
-                this.props.navigate('/english-learn/home')
+                this.props.navigate('/home')
             }).catch(
             (error) => {
                 setAuthHeader(null);
