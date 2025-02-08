5c32b24650891f95e16f742f362c623c67167db8
binh1922004 <binh1920042018@gmail.com>
2025-02-08 23:21:57 +0700

    add redis and generate OTP

---
+++ backend/src/main/java/learn/ELP/service/OtpGenerator.java
@@ -0,0 +1,14 @@
+package learn.ELP.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class OtpGenerator {
+    private static final Integer EXPIRE_TIME = 5;
+
+    public OtpGenerator() {
+        super();
+    }
+
+}
--- backend/src/main/java/learn/ELP/service/OtpService.java
+++ backend/src/main/java/learn/ELP/service/OtpService.java
@@ -4,12 +4,29 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class OtpService {
-    UserService userService;
+    private static final int OTP_EXPIRY_MINUTES = 5;
+    private static final String OTP_PREFIX = "otp:";
 
+    RedisTemplate<String, String> redisTemplate;
+    Random random = new Random();
+
+    public String generateOtp(String userName) {
+
+        String otp = String.valueOf(100000 + random.nextInt(900000)); // 6-digit OTP
+        String key = OTP_PREFIX + userName;
+        //save otp to redis
+        redisTemplate.opsForValue().set(key, otp, OTP_EXPIRY_MINUTES, TimeUnit.MINUTES);
+
+        return otp;
+    }
 }
